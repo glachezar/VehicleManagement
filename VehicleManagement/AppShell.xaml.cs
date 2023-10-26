68d087abc9d91c69434becdab573b2114fd530e7
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-10-26 15:01:02 +0300

    Add project files.

---
+++ VehicleManagement/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VehicleManagement;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ VehicleManagement/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace VehicleManagement;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
